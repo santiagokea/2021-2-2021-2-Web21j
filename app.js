ef36b2c75d2508e9d3ec29b94d4027ddf2017f98
Santiago Donoso <sand@kea.dk>
2021-08-27 11:15:17 +0200

    ...

---
+++ app.js
@@ -0,0 +1,21 @@
+var name = "Santiago"
+var lastName = "Donoso"
+// Hi, Santiago Donoso
+// String literal - Interpolation
+// Template literals
+// console.log(`Hi, ${name} ${lastName}`)
+// Normal concatenation
+// + is sum except when you start with 
+// a string
+// console.log("Hi, "+name+" "+lastName)
+console.log("Hi,",name,lastName)
+
+
+
+
+
+
+
+
+
+
